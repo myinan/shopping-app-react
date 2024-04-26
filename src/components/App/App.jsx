42e6ee90ba5a2d35147b4887c128fcd37674835f
Mustafa Yasir İnan <m.yasir.inan@gmail.com>
2024-04-26 22:16:16 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+}
+
+export default App;
