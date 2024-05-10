71599d5f06133387d2e3ae72daaaf6dda5f63277
myinan <m.yasir.inan@gmail.com>
2024-05-10 03:13:56 +0300

    Call useOutletContextData hook

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,42 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import useProductsData from "../hooks/useProductsData";
+import useOutletContextData from "../hooks/useContextData";
 
-function useContextData() {
-  const {
-    productsData: bootsData,
-    error: bootsError,
-    loading: bootsLoading,
-  } = useProductsData("Waterproof Boots");
-
-  const {
-    productsData: sportsData,
-    error: sportsError,
-    loading: sportsLoading,
-  } = useProductsData("sports shoes");
-
-  const {
-    productsData: featuredData,
-    error: featuredError,
-    loading: featuredLoading,
-  } = useProductsData("shoes popular");
-
-  return {
-    bootsData,
-    bootsError,
-    bootsLoading,
-    sportsData,
-    sportsError,
-    sportsLoading,
-    featuredData,
-    featuredError,
-    featuredLoading,
-  };
-}
+import { useEffect } from "react";
 
 export default function Layout() {
-  const contextObj = useContextData();
+  const contextObj = useOutletContextData();
+
+  useEffect(() => {
+    console.log("rendered");
+  });
 
   return (
     <>
