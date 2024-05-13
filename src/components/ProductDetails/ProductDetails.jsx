3d804584e8535d290f7a173864cb670b57f7d84d
myinan <m.yasir.inan@gmail.com>
2024-05-14 02:42:01 +0300

    Implement the ProductDetails page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,8 @@
-/* import styles from "./ProductDetails.module.css"; */
-import { useContext } from "react";
+import styles from "./ProductDetails.module.css";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ProductDataContext from "../../contexts/ProductDataContext";
+import ImgNotAvailable from "./assets/image-not-available.png";
 
 export default function ProductDetailsPage() {
   let { productId } = useParams();
@@ -15,5 +16,37 @@ export default function ProductDetailsPage() {
   const productsArray = [...dataB, ...dataS, ...dataF];
   const curElement = productsArray.find((product) => product.id === productId);
 
-  return <h1>{curElement.brand}</h1>;
+  useEffect(() => {
+    console.log(curElement);
+  });
+
+  return (
+    <main className={styles.productPage}>
+      <div className={styles.productImgContainer}>
+        {curElement.image &&
+        curElement.id !== "e87c0e9f-e3d3-4f3b-b0df-a165682bee4c" ? (
+          <img
+            src={curElement.image}
+            alt={curElement.title}
+            className={styles.productImg}
+          />
+        ) : (
+          <img
+            src={ImgNotAvailable}
+            alt={curElement.title}
+            className={styles.productImg}
+          />
+        )}
+      </div>
+      <div className={styles.productDetailsContainer}>
+        <h2 className={styles.productDetailTitle}>
+          {curElement.title.split(" ").slice(0, 4).join(" ")}
+        </h2>
+        <p className={styles.productDetailPrice}>{curElement.base_price}</p>
+        <p className={styles.productDetailDescription}>
+          {curElement.description}
+        </p>
+      </div>
+    </main>
+  );
 }
