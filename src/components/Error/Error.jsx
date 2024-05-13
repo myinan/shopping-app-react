3e67ab4d3a61d3341e15e02211d3446779f8358f
myinan <m.yasir.inan@gmail.com>
2024-05-14 00:49:44 +0300

    Implement the Error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,5 +1,39 @@
 import styles from "./Error.module.css";
+import catImg from "./assets/cat-error-image.jpeg";
+import { Link } from "react-router-dom";
 
 export default function Error() {
-  return <h1 className={styles.errorHeader}>404 Not Found</h1>;
+  return (
+    <main className={styles.errorPageTopContainer}>
+      <h3>404 - ERROR</h3>
+      <h1>OOPS!</h1>
+      <div className={styles.errorPageImgWrapper}>
+        <img
+          src={catImg}
+          alt="cat with a shoe"
+          className={styles.errorPageImg}
+        />
+      </div>
+      <p>THIS IS AWKWARD...</p>
+      <p>
+        This page either doesn&apos;t exist or is under construction. Sorry
+        about that, here&apos;s a picture of a cat instead.
+      </p>
+      <p>Try the links below!</p>
+      <div className={styles.errorPageLinksContainer}>
+        <Link to="/" className={styles.errorPageLink}>
+          HOME
+        </Link>
+        <Link to="/collection" className={styles.errorPageLink}>
+          COLLECTION
+        </Link>
+        <Link to="/sports" className={styles.errorPageLink}>
+          SPORTS
+        </Link>
+        <Link to="/boots" className={styles.errorPageLink}>
+          BOOTS
+        </Link>
+      </div>
+    </main>
+  );
 }
