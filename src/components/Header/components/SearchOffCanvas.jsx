56c931353f600df31c13476008eda74666d63d28
myinan <m.yasir.inan@gmail.com>
2024-05-16 03:08:17 +0300

    Style search button

--- src/components/Header/components/SearchOffCanvas.jsx
+++ src/components/Header/components/SearchOffCanvas.jsx
@@ -1,7 +1,6 @@
 import styles from "./SearchOffCanvas.module.css";
 import PropTypes from "prop-types";
 import { useState, useContext } from "react";
-import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import ProductDataContext from "../../../contexts/ProductDataContext";
 
@@ -51,9 +50,20 @@ function SearchOffCanvas() {
   };
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Search
-      </Button>
+      <button onClick={handleShow} className={styles.headerSearchButton}>
+        <svg
+          fill="#000000"
+          width="30px"
+          height="30px"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.027 9.92L16 13.95 14 16l-4.075-3.976A6.465 6.465 0 0 1 6.5 13C2.91 13 0 10.083 0 6.5 0 2.91 2.917 0 6.5 0 10.09 0 13 2.917 13 6.5a6.463 6.463 0 0 1-.973 3.42zM1.997 6.452c0 2.48 2.014 4.5 4.5 4.5 2.48 0 4.5-2.015 4.5-4.5 0-2.48-2.015-4.5-4.5-4.5-2.48 0-4.5 2.014-4.5 4.5z"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
 
       <Offcanvas
         show={show}
@@ -73,7 +83,7 @@ function SearchOffCanvas() {
               value={searchQuery}
               onChange={handleSearchChange}
             />
-            <button type="submit" className={styles.searchButton}>
+            <button type="submit" className={styles.innerButton}>
               GO
             </button>
           </form>
