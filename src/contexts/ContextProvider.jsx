8c2d31f1a7d24a5ed36fbabb727ec99ff079717a
myinan <m.yasir.inan@gmail.com>
2024-05-17 01:30:05 +0300

    Use default value of CartContext.Provider

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import ProductDataContext from "./ProductDataContext";
 import CartContext from "./CartContext";
 import useOutletContextData from "../hooks/useContextData";
@@ -6,13 +5,10 @@ import PropTypes from "prop-types";
 
 const ContextProvider = ({ children }) => {
   const productsData = useOutletContextData();
-  const [cart, setCart] = useState([]);
 
   return (
     <ProductDataContext.Provider value={productsData}>
-      <CartContext.Provider value={[cart, setCart]}>
-        {children}
-      </CartContext.Provider>
+      <CartContext.Provider>{children}</CartContext.Provider>
     </ProductDataContext.Provider>
   );
 };
