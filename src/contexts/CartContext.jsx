470743cf28ffe15cc307696ebd8c635f75b48a8d
myinan <m.yasir.inan@gmail.com>
2024-05-17 01:25:58 +0300

    Update default context object

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const CartContext = createContext(null);
+const CartContext = createContext([]);
 
 export default CartContext;
--- src/contexts/ProductDataContext.jsx
+++ src/contexts/ProductDataContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const ProductDataContext = createContext(null);
+const ProductDataContext = createContext({});
 
 export default ProductDataContext;
