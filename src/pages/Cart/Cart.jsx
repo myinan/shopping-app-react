e68ade60a2210c5bf8eb9a675dc009ebee1880ab
myinan <m.yasir.inan@gmail.com>
2024-05-17 02:28:20 +0300

    Dsiplay each added cart item's price

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,15 @@
-import styles from "./Cart.module.css";
+/* import styles from "./Cart.module.css"; */
+import { useContext } from "react";
+import CartContext from "../../contexts/CartContext";
 
 export default function CartPage() {
-  return <p>Cart Page</p>;
+  const [cart] = useContext(CartContext);
+
+  return (
+    <div>
+      {cart.map((item, index) => {
+        return <p key={index}>{item.price}</p>;
+      })}
+    </div>
+  );
 }
