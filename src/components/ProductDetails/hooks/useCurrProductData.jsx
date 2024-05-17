165a2bd56e0cb3cc72db26b95e9d094110fdcf57
myinan <m.yasir.inan@gmail.com>
2024-05-17 03:26:20 +0300

    Rename the component and add comments

--- src/components/ProductDetails/hooks/useCurrProductData.jsx
+++ src/components/ProductDetails/hooks/useCurrProductData.jsx
@@ -2,8 +2,10 @@ import { useContext, useState, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import ProductDataContext from "../../../contexts/ProductDataContext";
 
-export default function useVariantSelection() {
+export default function useCurrProductData() {
+  // Get produtId parameter to reach current product data
   let { productId } = useParams();
+
   const { bootsData, sportsData, featuredData } =
     useContext(ProductDataContext);
 
@@ -29,13 +31,11 @@ export default function useVariantSelection() {
 
   const [selectedVariant, setSelectedVariant] = useState(sortedVariantsArr[0]);
 
-  // The "effectDependency" dependency is required for correctly rendering the product price, when the
-  // ProductDetails page is rendered through the offCanvas. This dependency makes sure the selectedVariant
-  // is updated on parameter change, even if the ProductDetails component is still mounted.
-  const effectDependency = JSON.stringify([sortedVariantsArr, productId]);
+  // If the ProductDetails page is reached through the offCanvas, this useEffect is needed to render
+  // the correct data
   useEffect(() => {
     setSelectedVariant(sortedVariantsArr[0]);
-  }, [sortedVariantsArr, effectDependency]);
+  }, [sortedVariantsArr, productId]);
 
   // Event handler to update the selected variant object
   const handleVariantSelect = (event) => {
@@ -44,9 +44,9 @@ export default function useVariantSelection() {
   };
 
   return {
-    curElement,
-    sortedVariantsArr,
-    selectedVariant,
-    handleVariantSelect,
+    curElement, // The element in productsArray which it's id matches url "productId" parameter
+    sortedVariantsArr, // Avaliable price/size variants of the current shoe item, sorted
+    selectedVariant, // Selected price/size variant, initially the first item in sortedVariantsArr
+    handleVariantSelect, // handler function to select a different price/size variant
   };
 }
