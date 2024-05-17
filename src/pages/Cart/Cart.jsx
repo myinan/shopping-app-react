acbc2bb51a56ce2aa16c6ec51ca60e74d16d1189
myinan <m.yasir.inan@gmail.com>
2024-05-17 18:00:14 +0300

    Display items table and checkout btn

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,10 +5,23 @@ import PropTypes from "prop-types";
 import CartContext from "../../contexts/CartContext";
 
 function CartItem({ item }) {
+  const total = Number(item.price) * Number(item.quantity);
+
   return (
-    <div className={styles.cartItem}>
-      <p>{item.price}</p>
-    </div>
+    <tr className={styles.cartItemRow}>
+      <td className={styles.cartItemCell}>
+        <p>{item.title}</p>
+      </td>
+      <td className={styles.cartItemCell}>
+        <p>{`${item.price}$`}</p>
+      </td>
+      <td className={styles.cartItemCell}>
+        <p>{item.quantity}</p>
+      </td>
+      <td className={styles.cartItemCell}>
+        <p>{`${total}$`}</p>
+      </td>
+    </tr>
   );
 }
 
@@ -23,21 +36,51 @@ CartItem.propTypes = {
   }).isRequired,
 };
 
-export default function CartPage() {
+function FilledCartPage() {
   const [cart] = useContext(CartContext);
 
   return (
-    <div className={styles.cartContainer}>
-      {cart.length !== 0 ? (
-        cart.map((item) => {
-          return <CartItem item={item} key={item.id} />;
-        })
-      ) : (
-        <>
-          <h2>YOUR CART IS LOOKING EMPTY.</h2>
-          <Link to="/">SHOP NOW</Link>
-        </>
-      )}
+    <div className={styles.cartPage}>
+      <h1 className={styles.cartTitle}>YOUR CART</h1>
+
+      <form className={styles.cartForm}>
+        <table className={styles.cartTable}>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item) => {
+              return <CartItem item={item} key={item.id} />;
+            })}
+          </tbody>
+        </table>
+
+        <div className={styles.cartCheckout}>
+          <div className={styles.cartTotalPrice}>Total Price here...</div>
+          <button type="submit">CHECKOUT</button>
+          <Link to="/collection">Keep Shopping</Link>
+        </div>
+      </form>
     </div>
   );
+}
+
+function EmptyCartPage() {
+  return (
+    <div className={styles.cartPage}>
+      <h2>YOUR CART IS LOOKING EMPTY.</h2>
+      <Link to="/">SHOP NOW</Link>
+    </div>
+  );
+}
+
+export default function CartPage() {
+  const [cart] = useContext(CartContext);
+
+  return cart.length !== 0 ? <FilledCartPage /> : <EmptyCartPage />;
 }
