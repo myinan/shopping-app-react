1483cc2fa4cc7bb3c4042a2f26afcbb23a3c7654
myinan <m.yasir.inan@gmail.com>
2024-05-17 03:27:54 +0300

    Create state to control quantity selection, pass it to provider

--- src/components/ProductDetails/contexts/DetailContextProvider.jsx
+++ src/components/ProductDetails/contexts/DetailContextProvider.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
-import useVariantSelection from "../hooks/useVariantSelection";
+import { useState } from "react";
+import useCurrProductData from "../hooks/useCurrProductData";
 import ProductDetailContext from "./ProductDetailContext";
 
 const DetailContextProvider = ({ children }) => {
-  const data = useVariantSelection();
+  const data = useCurrProductData();
+  // Control the quantity selection input
+  const [selectedQuantity, setSelectedQuantity] = useState(1);
 
   return (
-    <ProductDetailContext.Provider value={data}>
+    <ProductDetailContext.Provider
+      value={{ ...data, selectedQuantity, setSelectedQuantity }}
+    >
       {children}
     </ProductDetailContext.Provider>
   );
